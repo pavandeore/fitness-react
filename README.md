d894d365f9f0d668e320c4d4f99a4811aa8c28b2
pawan deore <pavandeore90@gmail.com>
2020-12-23 15:47:02 +0530

    site update

--- README.md
+++
@@ -1,9 +0,0 @@
-STEPS TO EXECUTE ON YOUR MACHINE
-
-<ol>
-  <li> git clone https://github.com/pavandeore/fitness-react.git </li>
-  <li> cd fitness-react </li>
-  <li> npm install </li>
-  <li> npm start </li>
-  <li> And this will launch localhost:3000 on your browser. </li>
-</ol>
---
+++ precache-manifest.8d037df1969966164fb017aace24cbc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a68adf6708f97d419100d93fe90b22a",
+    "url": "/fitness-react/index.html"
+  },
+  {
+    "revision": "e037075f3fcc95a63082",
+    "url": "/fitness-react/static/css/main.7374b318.chunk.css"
+  },
+  {
+    "revision": "131bffe055bc3adf5947",
+    "url": "/fitness-react/static/js/2.886794ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fitness-react/static/js/2.886794ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e037075f3fcc95a63082",
+    "url": "/fitness-react/static/js/main.29404e55.chunk.js"
+  },
+  {
+    "revision": "2cb5dc9de4b824cc5e2d",
+    "url": "/fitness-react/static/js/runtime-main.6a8d2a22.js"
+  },
+  {
+    "revision": "8a9eb24bc0d7028fe2e41e9000bc9299",
+    "url": "/fitness-react/static/media/bg.8a9eb24b.jpg"
+  }
+]);
